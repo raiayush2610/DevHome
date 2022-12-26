2ea5b30a814949c852b9d9386826a25de9de2b97
Ayush Rai <ayushayushravi@gmail.com>
2022-12-26 15:14:05 +0530

    addingallPage

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ import Footer from './Component/Footer/footer'
 import Navbar from './Component/Navbar/Navbar';
 import Contact from './Component/pages/Contact/Contact';
 import Feature from './Component/pages/Home/Feature';
+import Plan from './Component/pages/Plan/Plan';
+import About from './Component/pages/Aboutus/About';
+import List from './Component/pages/ListProper/List'
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Carousel from './Component/pages/Carousel/Carousel';
 function App() {
@@ -19,9 +22,12 @@ function App() {
                 <Routes>
                   
                     <Route path = "/" element = {<Home />}/>
+                    <Route path = "/aboutus" element = {<About/>}/>
                     <Route path = "/contact" element = {<Contact />}/>
                     <Route path = "/Feature" element = {<Feature/>}/>
                     <Route path = "/caro" element = {<Carousel/>}/>
+                    <Route path = "/listprov" element = {<List/>}/>
+                    <Route path = "/plan" element = {<Plan/>}/>
                     <Route path = "/admin" element = {<Admin />}/>
                     <Route path= '*' element ={<NoPage/>}/>
                     
---
+++ src/Component/pages/Plan/Plan.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Plan() {
+  return (
+    <div>
+      <h1>tjis is plan page</h1>
+    </div>
+  )
+}
+
+export default Plan
